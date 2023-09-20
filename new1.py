844da347b54bad57f045c97fd6820a827e40b72f
Sharang-Emiza123 <sharang.naidu@emizainc.com>
2023-09-20 16:32:43 +0530

    20-09/02

--- new1.py
+++ new1.py
@@ -1,16 +1,12 @@
 import pandas as pd, statistics as st
 
-# df = pd.read_excel(r"C:\Users\Emiza\Downloads\SMconsumer-Aug23-SO.xlsx")
-df = pd.read_excel(r"D:\Users\shara\Downloads\SMconsumer-Aug23-SO.xlsx", parse_dates=['CreatedDate'])
+df = pd.read_excel(r"C:\Users\Emiza\Downloads\SMconsumer-Aug23-SO.xlsx")
+# df = pd.read_excel(r"D:\Users\shara\Downloads\SMconsumer-Aug23-SO.xlsx", parse_dates=['CreatedDate'])
 
 list1 = []
 list2 = []
 for i in range(len(df['soId'])-1):
     list1.append(df['soId'][i])
-
-
-maxItem = max(itemids, key=itemids.count)
-count = itemids.count(maxItem)
 
 dates = dict()
 
@@ -35,7 +31,7 @@ print(len(dates.keys()))
 # print(list2)
 
 # print("total products dispatched ",sum(df['TotalDispatched']))        
-print(df['CreatedDate'])
+# print(df['CreatedDate'])
 # print(df['CreatedDate'][0])
 # print("most sold SKU is {0} sold {1} items.".format(maxItem, count)) 
 # print("number of SOs processed -",len(list2))
