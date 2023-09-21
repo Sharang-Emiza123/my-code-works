10583b076d3fcfb1aa5a6ebe623e78c834502657
Sharang-Emiza123 <sharang.naidu@emizainc.com>
2023-09-21 17:28:40 +0530

    21-09/01

--- Python-project/app.py
+++ Python-project/app.py
@@ -5,15 +5,18 @@ import pandas as pd
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def login():
     return render_template('login.html')
 
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/home', methods=['POST', 'GET'])
 def HomePage():
     usrnm = request.args.get('Username')
     return render_template('homepage.html')
 
+@app.route('/exmgr', methods=['POST', 'GET'])
+def expMgr():
+    return 'redirecct successful !!'
 
 if __name__ == '__main__':
 	app.run(debug=True)
