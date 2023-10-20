6be4e355d133319fed920a32ce4ec62835201aa9
Sharang-Emiza123 <sharang.naidu@emizainc.com>
2023-10-20 15:00:29 +0530

    20-10/01

--- imgTOtxt.py
+++ imgTOtxt.py
@@ -5,7 +5,7 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Emiza\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 # Open an image using Pillow (PIL)
-image_path = r"C:\Users\Emiza\OneDrive\Desktop\test.jpg"  # Replace with the path to your image file
+image_path = r"C:\Users\Emiza\OneDrive\Desktop\test1.jpg"  # Replace with the path to your image file
 img = Image.open(image_path)
 
 # Perform OCR on the image
